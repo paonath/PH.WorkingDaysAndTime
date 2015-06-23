e9ad28a436b6c0a0bdf5fb8fd6b97fa76976f781
Paolo <paonath@gmail.com>
2015-06-23 11:11:47 +0200

    Add AddWorkingDays as ExtensionMethod
    Still a stub:  WorkingDateTimeExtension for DateTime Extension Methods, in order to move the calculation in a most appropriate class

---
+++ PH.WorkingDaysAndTime/PH.WorkingDaysAndTimeUtility.UnitTest/WorkingDateTimeExtensionUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PH.WorkingDaysAndTimeUtility.UnitTest
+{
+    [TestClass]
+    public class WorkingDateTimeExtensionUnitTest
+    {
+        [TestMethod]
+        public void Add_1_Day_To_Jun_23_2015_With_No_Holidays_Will_Return_Jun_24()
+        {
+            DateTime d = new DateTime(2015,6,23);
+            DateTime e = new DateTime(2015, 6, 24);
+            List<DayOfWeek> week = GetWorkWeek();
+            DateTime r = d.AddWorkingDays(1, new List<DateTime>(), week);
+
+            Assert.AreEqual(e,r);
+        }
+
+        [TestMethod]
+        public void Add_4_Day_To_Jun_23_2015_With_No_Holidays_Will_Return_Jun_29()
+        {
+            DateTime d = new DateTime(2015, 6, 23);
+            DateTime e = new DateTime(2015, 6, 29);
+            List<DayOfWeek> week = GetWorkWeek();
+            DateTime r = d.AddWorkingDays(4, new List<DateTime>(), week);
+
+            Assert.AreEqual(e, r);
+        }
+
+
+        private List<DayOfWeek> GetWorkWeek()
+        {
+            return new List<DayOfWeek>()
+            {
+                DayOfWeek.Monday , DayOfWeek.Tuesday , DayOfWeek.Wednesday
+                , DayOfWeek.Thursday , DayOfWeek.Friday
+            };
+        }
+    }
+}
---
+++ PH.WorkingDaysAndTime/PH.WorkingDaysAndTimeUtility/WorkingDateTimeExtension.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PH.WorkingDaysAndTimeUtility
+{
+    public static class WorkingDateTimeExtension
+    {
+        public static DateTime AddWorkingDays(this DateTime dateTime, int days
+            , List<DateTime> datesToExclude , List<DayOfWeek> workDaysOfTheWeeks 
+            )
+        {
+            DateTime end = dateTime;
+            for (int i = 0; i < days; i++)
+            {
+                bool moreAdd = true;
+                
+
+                while (moreAdd)
+                {
+                    int y = end.Year;
+                    end = end.AddDays(1);
+                    if (y < end.Year)
+                    {
+                        List<DateTime> newExclusions = new List<DateTime>();
+                        datesToExclude.ForEach(d =>
+                        {
+                            DateTime n;
+                            if (DateTime.TryParse(String.Format("{0}-{1}-{2}", end.Year, d.Month, d.Day), out n))
+                            {
+                                newExclusions.Add(n);
+                            }
+                        });
+
+                        datesToExclude.AddRange(newExclusions);
+                    }
+
+                    //check if current is a workingDay
+                    if (workDaysOfTheWeeks.Contains(end.DayOfWeek))
+                    {
+                        if (datesToExclude.Count == 0)
+                        {
+                            moreAdd = false;
+                        }
+                        else
+                        {
+                            var holiDayInList = datesToExclude.FirstOrDefault(x => x.Date == end.Date);
+                            moreAdd = !(holiDayInList > DateTime.MinValue);
+                            
+                        }
+
+                    }
+                }
+            }
+            return end;
+        }
+    }
+}
