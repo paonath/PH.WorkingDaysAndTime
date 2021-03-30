79d585a81adca497da36112d02ac9fa2fd72f942
Paolo Innocenti <paonath@gmail.com>
2021-03-30 08:19:52 +0200

    new testing

--- PH.WorkingDaysAndTime/PH.WorkingDaysAndTimeUtility.UnitTest/GetWorkingDaysBetweenTwoDateTimesUnitTest.cs
+++ PH.WorkingDaysAndTime/PH.WorkingDaysAndTimeUtility.UnitTest/GetWorkingDaysBetweenTwoDateTimesUnitTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PH.WorkingDaysAndTimeUtility.Configuration;
 using Xunit;
 
 namespace PH.WorkingDaysAndTimeUtility.UnitTest
@@ -116,5 +117,57 @@ namespace PH.WorkingDaysAndTimeUtility.UnitTest
         }
 
 
+
+        [Fact]
+        public void More_Get_WorkingDays()
+        {
+            var start = new DateTime(2021, 1, 1);
+            var end = new DateTime(2021, 12, 31);
+            
+            //this is the configuration of a work-week: 8h/day from monday to friday
+            var wts1 = new WorkTimeSpan() 
+                { Start = new TimeSpan(9, 0, 0), End = new TimeSpan(13, 0, 0) };
+            var wts2 = new WorkTimeSpan() 
+                { Start = new TimeSpan(14, 0, 0), End = new TimeSpan(18, 0, 0) };
+            var wts = new List<WorkTimeSpan>() { wts1, wts2 };
+
+            var week = new WeekDaySpan()
+            {
+                WorkDays = new Dictionary<DayOfWeek, WorkDaySpan>()
+                {
+                    {DayOfWeek.Monday, new WorkDaySpan() {TimeSpans = wts}}
+                    ,
+                    {DayOfWeek.Tuesday, new WorkDaySpan() {TimeSpans = wts}}
+                    ,
+                    {DayOfWeek.Wednesday, new WorkDaySpan() {TimeSpans = wts}}
+                    ,
+                    {DayOfWeek.Thursday, new WorkDaySpan() {TimeSpans = wts}}
+                    ,
+                    {DayOfWeek.Friday, new WorkDaySpan() {TimeSpans = wts}}
+                }
+            };
+
+            //this is the configuration for holidays: 
+            //in Italy we have this list of Holidays plus 1 day different on each province,
+            //for mine is 1 Dec (see last element of the List<AHolyDay>).
+            var italiansHoliDays = new List<AHolyDay>()
+            {
+                new EasterMonday(),new HoliDay(1, 1),new HoliDay(6, 1),
+                new HoliDay(25, 4),new HoliDay(1, 5),new HoliDay(2, 6),
+                new HoliDay(15, 8),new HoliDay(1, 11),new HoliDay(8, 12),
+                new HoliDay(25, 12),new HoliDay(26, 12)
+                , new HoliDay(1, 12)
+            };
+
+            //instantiate with configuration
+            var utility = new WorkingDaysAndTimeUtility(week, italiansHoliDays);
+
+            var workDays = utility.GetWorkingDaysBetweenTwoDateTimes(start, end, true);
+
+
+            var dbg = workDays.FirstOrDefault();
+            Assert.NotEqual(start, dbg);
+
+        }
     }
 }
